84d388f169b3b39e40dc9441999cc3774c90622c
yelemi <71648655+yelemi@users.noreply.github.com>
2020-09-21 10:16:13 -0400

    Add files via upload
    yelemi olorunleke

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,23 +1,46 @@
-import java.util.Scanner;
 import java.util.Random;
+import java.util.Scanner;
 
 public class GuessingGame {
-    public static void main(String[] args) {
-
-         Random rand = new Random();
-         int randguess = rand.nextInt(101);
-
-        System.out.println("Enter an integer: ");
-        Scanner kb = new Scanner(System.in);
-        int userguess = kb.nextInt();
-
-        while (userguess != -1) {
-            System.out.println("Wrong number, please enter another one");
-            userguess = kb.nextInt();
-            if (userguess == randguess) {
-                System.out.println("You guessed right, you won!");
-                break;
-            }
-        }
-    }
+	public static void main(String[]args) {
+		
+		Random rand = new Random();
+		int upperbound = 100;
+		
+		int guessNum = rand.nextInt(upperbound);
+		
+		Scanner sc = new Scanner(System.in);
+		
+		//how many
+		System.out.println("Enter how many trials you want:");
+		int k = sc.nextInt();
+		
+		int i;
+		int selectNum = 0;
+		int specialNum = -1;
+		
+		for( i = 0; i < k; i++) {
+			
+		System.out.println("Choose an integer between 1-100:");
+		selectNum = sc.nextInt();
+	
+			
+			if ( guessNum == selectNum) {
+				System.out.println("Congratulations, you guess the number");
+				System.exit(0);
+				break;
+			}
+	
+			else if ( guessNum > selectNum && i != k -1) {
+				System.out.println("The Answer is Greater than "+ selectNum);
+			}
+			
+			else if ( guessNum < selectNum && i != k-1) {
+				System.out.println("The Answer is Less than "+ selectNum);
+			}
+			
+			}
+		System.out.println("The correct answer is "+ guessNum+ " :(");
+		
+	}
 }
